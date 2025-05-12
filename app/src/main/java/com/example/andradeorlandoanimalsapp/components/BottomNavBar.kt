be51cfabcccc6b878c81ff91b2461785d5fd8fee
unknown <orlandoabta@gmail.com>
2025-05-12 08:30:14 -0600

    Navegador 2

---
+++ app/src/main/java/com/example/andradeorlandoanimalsapp/components/BottomNavBar.kt
@@ -0,0 +1,35 @@
+package com.example.andradeorlandoanimalsapp.components
+
+// components/BottomNavBar.kt
+
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Place
+
+@Composable
+fun BottomNavBar(navController: NavController) {
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+
+    BottomNavigation(
+        backgroundColor = Color(0xFF2D3B39), // Verde oscuro
+        contentColor = Color.White
+    ) {
+        BottomNavigationItem(
+            icon = { Icon(Icons.Default.Home, contentDescription = "Inicio") },
+            label = { Text("Inicio") },
+            selected = currentRoute == "listaAnimales",
+            onClick = { navController.navigate("listaAnimales") }
+        )
+        BottomNavigationItem(
+            icon = { Icon(Icons.Default.Place, contentDescription = "Ambientes") },
+                    label = { Text("Ambientes") },
+            selected = currentRoute == "listaAmbientes",
+            onClick = { navController.navigate("listaAmbientes") }
+        )
+    }
+}
--- app/src/main/java/com/example/andradeorlandoanimalsapp/screens/ListaAnimales.kt
+++ app/src/main/java/com/example/andradeorlandoanimalsapp/screens/ListaAnimales.kt
@@ -1,2 +1,4 @@
 package com.example.andradeorlandoanimalsapp.screens
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
