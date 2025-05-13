376b9e39ba7992e5e7d1f7d0d16a839c1ffa1ec6
unknown <orlandoabta@gmail.com>
2025-05-13 05:56:26 -0600

    funcionalidad 1

--- app/src/main/java/com/example/andradeorlandoanimalsapp/components/BottomNavBar.kt
+++ app/src/main/java/com/example/andradeorlandoanimalsapp/components/BottomNavBar.kt
@@ -4,51 +4,86 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Pets
 import androidx.compose.material.icons.filled.Landscape
+import androidx.compose.material.icons.filled.Pets
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.andradeorlandoanimalsapp.screens.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 
 @Composable
 fun BottomNavBar(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
-            .height(56.dp)
-            .clip(RoundedCornerShape(50))
-            .background(Color(0xFFDEE094))
+            .padding(bottom = 24.dp)
     ) {
-        NavigationBar(
-            containerColor = Color.Transparent,
-            tonalElevation = 0.dp
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.6f)
+                .height(56.dp)
+                .align(Alignment.Center)
+                .clip(RoundedCornerShape(50))
+                .background(Color(0xFFDEE094)),
+            contentAlignment = Alignment.Center
         ) {
-            // Icono de Animales
-            NavigationBarItem(
-                selected = false,
-                onClick = { navController.navigate("listaAnimales") },
-                icon = { Icon(Icons.Filled.Pets, contentDescription = "Animales", tint = Color.Black) }, // Icono de animales
-                label = { Text("Inicio") }
-            )
-            // Icono de Ambientes
-            NavigationBarItem(
-                selected = false,
-                onClick = { navController.navigate("listaAmbientes") },
-                icon = { Icon(Icons.Filled.Landscape, contentDescription = "Ambientes", tint = Color.Black) }, // Icono de árbol
-                label = { Text("Ambientes") }
-            )
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                NavigationBarItem(
+                    selected = false,
+                    onClick = { navController.navigate("listaAnimales") },
+                    icon = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Icon(
+                                Icons.Filled.Pets,
+                                contentDescription = "Animales",
+                                tint = Color.Black
+                            )
+                            Spacer(modifier = Modifier.width(6.dp))
+                            Text("Inicio", color = Color.Black, fontSize = 16.sp)
+                        }
+                    },
+                    label = null
+                )
+                NavigationBarItem(
+                    selected = false,
+                    onClick = { navController.navigate("listaAmbientes") },
+                    icon = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Icon(
+                                Icons.Filled.Landscape,
+                                contentDescription = "Ambientes",
+                                tint = Color.Black
+                            )
+                            Spacer(modifier = Modifier.width(6.dp))
+                            Text("Ambientes", color = Color.Black, fontSize = 16.sp)
+                        }
+                    },
+                    label = null
+                )
+            }
         }
     }
 }
+
 
 @Composable
 fun AppNavigation(navController: NavHostController) {
--- app/src/main/java/com/example/andradeorlandoanimalsapp/screens/ListaAnimales.kt
+++ app/src/main/java/com/example/andradeorlandoanimalsapp/screens/ListaAnimales.kt
@@ -5,17 +5,21 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
 import com.example.andradeorlandoanimalsapp.model.Animal
 import com.example.andradeorlandoanimalsapp.network.ApiClient
-
 @Composable
 fun ListaAnimales(navController: NavHostController) {
     var animales by remember { mutableStateOf<List<Animal>>(emptyList()) }
@@ -36,13 +40,49 @@ fun ListaAnimales(navController: NavHostController) {
             CircularProgressIndicator()
         }
     } else {
-        LazyColumn {
-            items(animales) { animal ->
-                AnimalItem(animal, navController)
+        Column(modifier = Modifier.fillMaxSize()) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Animales",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Button(
+                    onClick = {  },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFDEE094))
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Agregar",
+                        tint = Color.Black
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text("Agregar", color = Color.Black)
+                }
+            }
+
+            Text(
+                text = "Conoce a los animales más increíbles del mundo",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 8.dp)
+            )
+            LazyColumn {
+                items(animales) { animal ->
+                    AnimalItem(animal, navController)
+                }
             }
         }
     }
 }
+
 @Composable
 fun AnimalItem(animal: Animal, navController: NavHostController) {
     Card(
@@ -51,28 +91,27 @@ fun AnimalItem(animal: Animal, navController: NavHostController) {
             .padding(8.dp)
             .clickable {
                 navController.navigate("detalleAnimal/${animal.id}")
-            }
+            },
+        shape = RoundedCornerShape(16.dp)
     ) {
-        Column {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(8.dp)
+        ) {
             Image(
                 painter = rememberAsyncImagePainter(animal.image),
                 contentDescription = animal.name,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
+                    .size(200.dp)
+                    .clip(RoundedCornerShape(16.dp))
             )
+            Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = animal.name,
                 style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(8.dp)
-            )
-            Text(
-                text = animal.description,
-                style = MaterialTheme.typography.bodyMedium,
-                modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+                color = MaterialTheme.colorScheme.onBackground
             )
         }
     }
 }
-
